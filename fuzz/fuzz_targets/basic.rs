47d0018e3109517e0520cb95ebbe34bc3bd595e8
Jan Verbeek <jan.verbeek@posteo.nl>
2021-10-13 13:48:49 +0200

    fuzz: basic: Update API usage

--- fuzz/fuzz_targets/basic.rs
+++ fuzz/fuzz_targets/basic.rs
@@ -1,24 +1,22 @@
 #![no_main]
 use libfuzzer_sys::fuzz_target;
 
-use std::ffi::OsStr;
-use std::os::unix::ffi::OsStrExt;
+use std::str::from_utf8;
 
 use unicode_width::{UnicodeWidthChar, UnicodeWidthStr};
 
-use os_display::{Quotable, Quoted};
+use os_display::Quoted;
 
 const QUOTE_STARTS: &[char] = &['\'', '"', '$', '\\'];
 
 fuzz_target!(|data: &[u8]| {
-    let text = OsStr::from_bytes(data);
-    let quoted = text.quote().to_string();
-    let maybe_quoted = text.maybe_quote().to_string();
+    let quoted = Quoted::unix_raw(data).to_string();
+    let maybe_quoted = Quoted::unix_raw(data).force(false).to_string();
     assert!(quoted.starts_with(QUOTE_STARTS));
     if maybe_quoted.starts_with(QUOTE_STARTS) {
         assert_eq!(quoted, maybe_quoted);
     } else {
-        let text = text.to_str().expect("should be valid unicode");
+        let text = from_utf8(data).expect("should be valid unicode");
         assert_eq!(maybe_quoted, text);
         assert!(!text.as_bytes().iter().any(u8::is_ascii_whitespace));
         assert!(!text.is_empty());
@@ -30,7 +28,7 @@ fuzz_target!(|data: &[u8]| {
         assert!(!case.contains('\n'), "{:?}", case);
     }
 
-    if let Some(text) = text.to_str() {
+    if let Ok(text) = from_utf8(data) {
         let quoted = Quoted::windows(text).to_string();
         let maybe_quoted = Quoted::windows(text).force(false).to_string();
         assert!(quoted.starts_with(&['\'', '"'][..]));
