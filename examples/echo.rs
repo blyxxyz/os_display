5db5ecc627cc2fac83d98614829af62774c40e1e
Jan Verbeek <jan.verbeek@posteo.nl>
2021-10-14 16:02:58 +0200

    Fix echo example feature usage

--- examples/echo.rs
+++ examples/echo.rs
@@ -3,15 +3,17 @@ use os_display::Quotable;
 fn main() {
     for arg in std::env::args_os().skip(1) {
         println!("Native: {}", arg.maybe_quote());
-        #[cfg(any(feature = "windows", feature = "unix"))]
-        if let Some(arg) = arg.to_str() {
-            use os_display::Quoted;
-            #[cfg(feature = "unix")]
-            #[cfg(windows)]
-            println!("Unix: {}", Quoted::unix(arg).force(false));
-            #[cfg(feature = "windows")]
-            #[cfg(not(windows))]
-            println!("Windows: {}", Quoted::windows(arg).force(false));
+        #[cfg(all(windows, feature = "unix"))]
+        {
+            if let Some(arg) = arg.to_str() {
+                println!("Unix: {}", os_display::Quoted::unix(arg).force(false));
+            }
+        }
+        #[cfg(all(not(windows), feature = "windows"))]
+        {
+            if let Some(arg) = arg.to_str() {
+                println!("Windows: {}", os_display::Quoted::windows(arg).force(false));
+            }
         }
     }
 }
