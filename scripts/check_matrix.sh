8baa3be2741336ad16dde5eff854d69dd528ee8d
Jan Verbeek <jan.verbeek@posteo.nl>
2025-03-28 11:19:02 +0100

    Test WASI in the CI
    We essentially have three platform flavors: Windows, Unix, and
    opaque. We now treat WASI as opaque which makes it a useful platform
    to test on.
    
    We no longer need the horrible old WASM hack.

--- scripts/check_matrix.sh
+++ scripts/check_matrix.sh
@@ -1,33 +1,38 @@
 #!/bin/sh
 # Check if all the different combinations of enabled features compile.
-# I usually run this command:
-
-# rm Cargo.lock; scripts/check_matrix.sh && scripts/check_matrix.sh --target x86_64-pc-windows-msvc && scripts/check_matrix.sh --target wasm32-unknown-unknown
-
-# (Older versions have trouble with modern Cargo.locks.)
+# Invoke like `scripts/check_matrix.sh +1.66`, `scripts/check_matrix.sh --target x86_64-pc-windows-msvc`.
+# You may have to `rm Cargo.lock` first.
 
 set -e
+
+version='+stable'
+case "$1" in
+    +*)
+    version="$1"
+    shift
+esac
+echo "version=$version"
+echo "args=$@"
+
 set -v
 
-# 1.68 really should be 1.66 but that one can't handle modern cargo registries
+cargo "$version" check --no-default-features --features native "$@"
+cargo "$version" check --no-default-features --features native,std "$@"
 
-cargo +1.68 check --no-default-features --features native "$@"
-cargo +1.68 check --no-default-features --features native,std "$@"
+cargo "$version" check --no-default-features --features unix "$@"
+cargo "$version" check --no-default-features --features unix,std "$@"
 
-cargo +1.68 check --no-default-features --features unix "$@"
-cargo +1.68 check --no-default-features --features unix,std "$@"
+cargo "$version" check --no-default-features --features windows "$@"
+cargo "$version" check --no-default-features --features windows,std "$@"
 
-cargo +1.68 check --no-default-features --features windows "$@"
-cargo +1.68 check --no-default-features --features windows,std "$@"
+cargo "$version" check --no-default-features --features native,unix "$@"
+cargo "$version" check --no-default-features --features native,unix,std "$@"
 
-cargo +1.68 check --no-default-features --features native,unix "$@"
-cargo +1.68 check --no-default-features --features native,unix,std "$@"
+cargo "$version" check --no-default-features --features native,windows "$@"
+cargo "$version" check --no-default-features --features native,windows,std "$@"
 
-cargo +1.68 check --no-default-features --features native,windows "$@"
-cargo +1.68 check --no-default-features --features native,windows,std "$@"
+cargo "$version" check --no-default-features --features unix,windows "$@"
+cargo "$version" check --no-default-features --features unix,windows,std "$@"
 
-cargo +1.68 check --no-default-features --features unix,windows "$@"
-cargo +1.68 check --no-default-features --features unix,windows,std "$@"
-
-cargo +1.68 check --no-default-features --features native,unix,windows "$@"
-cargo +1.68 check --no-default-features --features native,unix,windows,std "$@"
+cargo "$version" check --no-default-features --features native,unix,windows "$@"
+cargo "$version" check --no-default-features --features native,unix,windows,std "$@"
--- scripts/test_matrix.sh
+++ scripts/test_matrix.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
 set -e
+
+version='+stable'
+case "$1" in
+    +*)
+    version="$1"
+    shift
+esac
+echo "version=$version"
+echo "args=$@"
+
 set -v
 
-cargo +1.68 test --no-default-features --features native,std --lib "$@"
-cargo +1.68 test --no-default-features --features unix,std --lib "$@"
-cargo +1.68 test --no-default-features --features windows,std --lib "$@"
-cargo +1.68 test --no-default-features --features native,unix,std --lib "$@"
-cargo +1.68 test --no-default-features --features native,windows,std --lib "$@"
-cargo +1.68 test --no-default-features --features unix,windows,std --lib "$@"
-cargo +1.68 test --no-default-features --features native,unix,windows,std --lib "$@"
+cargo "$version" test --no-default-features --features native,std --lib "$@"
+cargo "$version" test --no-default-features --features unix,std --lib "$@"
+cargo "$version" test --no-default-features --features windows,std --lib "$@"
+cargo "$version" test --no-default-features --features native,unix,std --lib "$@"
+cargo "$version" test --no-default-features --features native,windows,std --lib "$@"
+cargo "$version" test --no-default-features --features unix,windows,std --lib "$@"
+cargo "$version" test --no-default-features --features native,unix,windows,std --lib "$@"
